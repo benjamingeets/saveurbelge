0c9f88e48c6793ea38773778b6ebcc4098fdbdd4
Benjamin Geets <benjamingeets@tutamail.com>
2021-08-12 23:28:32 +0200

    Update Search.svelte

--- src/lib/index/Search.svelte
+++ src/lib/index/Search.svelte
@@ -5,7 +5,7 @@ import Button from "$lib/Button.svelte";
 <div id="search" class="flex flex-col justify-center items-center rounded-md py-80">
     <p class="uppercase text-white text-5xl mb-10 font-bebas">Quel type de commerce cherchez-vous ?</p>
     <form class="flex justify-between bg-white p-2 rounded-md w-full max-w-3xl">
-        <div class="w-6/12 bg-grey-light flex justify-between p-2" >
+        <div class="w-6/12 bg-grey-light flex justify-between p-2 rounded-md" >
             <input class="bg-grey-light w-full" type="text" placeholder="7500 Tournai">
             <span>📍</span>
         </div>
