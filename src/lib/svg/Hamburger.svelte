22fb1626383240e32be78d15d7451982d08d16df
Benjamin Geets <benjamingeets@tutamail.com>
2021-08-12 23:28:02 +0200

    Create Hamburger.svelte

---
+++ src/lib/svg/Hamburger.svelte
@@ -0,0 +1,5 @@
+<svg on:click class="cursor-pointer" width="29" height="23" viewBox="0 0 29 23" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M2 2H27" stroke="#502419" stroke-width="3" stroke-linecap="round" stroke-linejoin="round"/>
+<path d="M2 11.375H27" stroke="#502419" stroke-width="3" stroke-linecap="round" stroke-linejoin="round"/>
+<path d="M2 20.75H27" stroke="#502419" stroke-width="3" stroke-linecap="round" stroke-linejoin="round"/>
+</svg>
