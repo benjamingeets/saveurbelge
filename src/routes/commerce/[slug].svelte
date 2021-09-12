76f5353de166e3c306e78d8380f838b048f9396f
Benjamin Geets <benjamingeets@tutamail.com>
2021-09-13 00:09:03 +0200

    Update [slug].svelte

--- src/routes/commerce/[slug].svelte
+++ src/routes/commerce/[slug].svelte
@@ -46,7 +46,7 @@ import Badges from '$lib/Badges.svelte'
 <link rel="stylesheet" href="https://unpkg.com/leaflet@1.7.1/dist/leaflet.css"
 integrity="sha512-xodZBNTC5n17Xt2atTPuE1HxjVMSvLVW9ocqUKLsCC5CXdbqCmblAshOMAS6/keqq/sMZMZ19scR4PsZChSR7A=="
 crossorigin=""/>
-<Head title={`${shop.name} à ${shop.address.city} - SaveurBelge`} description={shop.description} lien={`commerce/${shop.slug}`} image={shop.header ? `https://${API}/public/${shop._id}_header.webp` : `${DOMAIN}/public/image/share.png`}/>
+<Head title={`${shop.name} à ${shop.address.city} - SaveurBelge`} description={shop.description} lien={`commerce/${shop.slug}`} image={shop.header ? `${API}/public/${shop._id}_header.webp` : `${DOMAIN}/public/image/share.png`}/>
 <main class="flex flex-col items-center w-full">
     <div class="w-full rounded-md h-60" style={shop.header ? `background-image:url(${API}/public/${shop._id}_header.webp);background-size:cover;background-position:center` :`background-image:url(${DOMAIN}/images/headers/${shop.sector}.png)`} >
 
