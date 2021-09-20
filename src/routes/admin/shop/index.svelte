744252252c143615184becb9395d7b4a44697e6f
Benjamin Geets <benjamingeets@tutamail.com>
2021-09-20 20:35:02 +0200

    add search bar

--- src/routes/admin/shop/index.svelte
+++ src/routes/admin/shop/index.svelte
@@ -3,19 +3,33 @@ import { getAllShops } from "$lib/admin_req";
 import {currentAdminPage} from '$lib/store.js'
 import { onMount } from "svelte";
 let shops
+let searchInput
 let display = false
+
+let displayShops = []
     onMount(async()=>{
         currentAdminPage.update(n => "shop")
         shops = await getAllShops()
         shops.sort((a,b)=>{
             return a.status - b.status
         })
+        displayShops = shops
         display = true
     })
+const search = () =>{
+    displayShops = shops.filter(shop=>shop.name.toLowerCase().includes(searchInput.toLowerCase()))
+
+}
 </script>
 
 {#if display}
-    {#each shops as shop}
+    <section>
+        <p>Recherche:</p>
+        <input type="text" class="input-normal" bind:value={searchInput} on:input={()=>{search()}}>
+
+        <hr class="my-4">
+    </section>
+    {#each displayShops as shop}
     <a href={`/admin/shop/${shop._id}`} class="flex justify-between border my-2 rounded-md p-2 max-w-3xl w-full hover:bg-grey-light">
         <div>
             🚜 {shop.name} ({shop.slug})
