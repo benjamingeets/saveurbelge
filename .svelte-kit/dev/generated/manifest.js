19d7b5a97b0f8887c9107a0f21f13ffeb6366563
Benjamin Geets <benjamingeets@tutamail.com>
2021-09-20 20:35:14 +0200

    Update manifest.js

--- .svelte-kit/dev/generated/manifest.js
+++ .svelte-kit/dev/generated/manifest.js
@@ -2,6 +2,7 @@ const c = [
 	() => import("../../../src/routes/__layout.svelte"),
 	() => import("../../../src/routes/__error.svelte"),
 	() => import("../../../src/routes/index.svelte"),
+	() => import("../../../src/routes/politique-de-confidentialite.svelte"),
 	() => import("../../../src/routes/ajouter-mon-commerce.svelte"),
 	() => import("../../../src/routes/deconnexion.svelte"),
 	() => import("../../../src/routes/commercant/__layout.reset.svelte"),
@@ -34,74 +35,77 @@ export const routes = [
 	// src/routes/index.svelte
 	[/^\/$/, [c[0], c[2]], [c[1]]],
 
+	// src/routes/politique-de-confidentialite.svelte
+	[/^\/politique-de-confidentialite\/?$/, [c[0], c[3]], [c[1]]],
+
 	// src/routes/ajouter-mon-commerce.svelte
-	[/^\/ajouter-mon-commerce\/?$/, [c[0], c[3]], [c[1]]],
+	[/^\/ajouter-mon-commerce\/?$/, [c[0], c[4]], [c[1]]],
 
 	// src/routes/deconnexion.svelte
-	[/^\/deconnexion\/?$/, [c[0], c[4]], [c[1]]],
+	[/^\/deconnexion\/?$/, [c[0], c[5]], [c[1]]],
 
 	// src/routes/sitemap.xml.js
 	[/^\/sitemap\.xml$/],
 
 	// src/routes/commercant/index.svelte
-	[/^\/commercant\/?$/, [c[5], c[6]], []],
+	[/^\/commercant\/?$/, [c[6], c[7]], []],
 
 	// src/routes/commercant/edit-password.svelte
-	[/^\/commercant\/edit-password\/?$/, [c[5], c[7]], []],
+	[/^\/commercant\/edit-password\/?$/, [c[6], c[8]], []],
 
 	// src/routes/commercant/informations.svelte
-	[/^\/commercant\/informations\/?$/, [c[5], c[8]], []],
+	[/^\/commercant\/informations\/?$/, [c[6], c[9]], []],
 
 	// src/routes/commercant/contact.svelte
-	[/^\/commercant\/contact\/?$/, [c[5], c[9]], []],
+	[/^\/commercant\/contact\/?$/, [c[6], c[10]], []],
 
 	// src/routes/commercant/shop/new.svelte
-	[/^\/commercant\/shop\/new\/?$/, [c[5], c[10]], []],
+	[/^\/commercant\/shop\/new\/?$/, [c[6], c[11]], []],
 
 	// src/routes/commercant/shop/[id].svelte
-	[/^\/commercant\/shop\/([^/]+?)\/?$/, [c[5], c[11]], [], (m) => ({ id: d(m[1])})],
+	[/^\/commercant\/shop\/([^/]+?)\/?$/, [c[6], c[12]], [], (m) => ({ id: d(m[1])})],
 
 	// src/routes/validation/mot-de-passe.svelte
-	[/^\/validation\/mot-de-passe\/?$/, [c[0], c[12]], [c[1]]],
+	[/^\/validation\/mot-de-passe\/?$/, [c[0], c[13]], [c[1]]],
 
 	// src/routes/validation/compte.svelte
-	[/^\/validation\/compte\/?$/, [c[0], c[13]], [c[1]]],
+	[/^\/validation\/compte\/?$/, [c[0], c[14]], [c[1]]],
 
 	// src/routes/validation/mail.svelte
-	[/^\/validation\/mail\/?$/, [c[0], c[14]], [c[1]]],
+	[/^\/validation\/mail\/?$/, [c[0], c[15]], [c[1]]],
 
 	// src/routes/connexion.svelte
-	[/^\/connexion\/?$/, [c[0], c[15]], [c[1]]],
+	[/^\/connexion\/?$/, [c[0], c[16]], [c[1]]],
 
 	// src/routes/recherche.svelte
-	[/^\/recherche\/?$/, [c[0], c[16]], [c[1]]],
+	[/^\/recherche\/?$/, [c[0], c[17]], [c[1]]],
 
 	// src/routes/a-propos.svelte
-	[/^\/a-propos\/?$/, [c[0], c[17]], [c[1]]],
+	[/^\/a-propos\/?$/, [c[0], c[18]], [c[1]]],
 
 	// src/routes/commerce/[slug].svelte
-	[/^\/commerce\/([^/]+?)\/?$/, [c[0], c[18]], [c[1]], (m) => ({ slug: d(m[1])})],
+	[/^\/commerce\/([^/]+?)\/?$/, [c[0], c[19]], [c[1]], (m) => ({ slug: d(m[1])})],
 
 	// src/routes/contact.svelte
-	[/^\/contact\/?$/, [c[0], c[19]], [c[1]]],
+	[/^\/contact\/?$/, [c[0], c[20]], [c[1]]],
 
 	// src/routes/admin/index.svelte
-	[/^\/admin\/?$/, [c[20], c[21]], []],
+	[/^\/admin\/?$/, [c[21], c[22]], []],
 
 	// src/routes/admin/utilisateur/[id].svelte
-	[/^\/admin\/utilisateur\/([^/]+?)\/?$/, [c[20], c[22]], [], (m) => ({ id: d(m[1])})],
+	[/^\/admin\/utilisateur\/([^/]+?)\/?$/, [c[21], c[23]], [], (m) => ({ id: d(m[1])})],
 
 	// src/routes/admin/categories.svelte
-	[/^\/admin\/categories\/?$/, [c[20], c[23]], []],
+	[/^\/admin\/categories\/?$/, [c[21], c[24]], []],
 
 	// src/routes/admin/commerces.svelte
-	[/^\/admin\/commerces\/?$/, [c[20], c[24]], []],
+	[/^\/admin\/commerces\/?$/, [c[21], c[25]], []],
 
 	// src/routes/admin/shop/index.svelte
-	[/^\/admin\/shop\/?$/, [c[20], c[25]], []],
+	[/^\/admin\/shop\/?$/, [c[21], c[26]], []],
 
 	// src/routes/admin/shop/[id].svelte
-	[/^\/admin\/shop\/([^/]+?)\/?$/, [c[20], c[26]], [], (m) => ({ id: d(m[1])})],
+	[/^\/admin\/shop\/([^/]+?)\/?$/, [c[21], c[27]], [], (m) => ({ id: d(m[1])})],
 
 	// src/routes/api/get-categories.js
 	[/^\/api\/get-categories\/?$/],
