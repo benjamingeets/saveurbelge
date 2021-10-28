0e4f92e50681960a105e18695497f69f91cbdfab
Benjamin Geets <benjamingeets@tutamail.com>
2021-10-28 16:29:12 +0200

    Update Map.svelte

--- src/lib/index/Map.svelte
+++ src/lib/index/Map.svelte
@@ -14,7 +14,7 @@
     }
     onMount(async()=>{
         L = await import('leaflet')
-        const map = L.map('map').setView([lat, lon], 8);
+        const map = L.map('map',{scrollWheelZoom: false}).setView([lat, lon], 8);
         L.tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png', {
             attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
             subdomains: 'abcd',
