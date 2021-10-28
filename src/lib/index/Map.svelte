4d6020ac676a432f7668cdb1447b1733d509f9db
Benjamin Geets <benjamingeets@tutamail.com>
2021-10-28 16:24:31 +0200

    Update Map.svelte

--- src/lib/index/Map.svelte
+++ src/lib/index/Map.svelte
@@ -30,6 +30,6 @@
 <link rel="stylesheet" href="https://unpkg.com/leaflet@1.7.1/dist/leaflet.css"
 integrity="sha512-xodZBNTC5n17Xt2atTPuE1HxjVMSvLVW9ocqUKLsCC5CXdbqCmblAshOMAS6/keqq/sMZMZ19scR4PsZChSR7A=="
 crossorigin=""/>
-<div class="h-96 w-full max-w-5xl rounded-md" id="map">
+<div class="h-96 w-full max-w-5xl rounded-md z-0" id="map">
 
 </div>
