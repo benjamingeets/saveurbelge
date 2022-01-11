68ed2733fc406fc65efe37c0ea267372db1f9c78
Benjamin Geets <benjamin@Benjamins-Mac-mini.local>
2022-01-11 11:17:54 +0100

    feat: install tailwind and create routes

--- start/routes.ts
+++ start/routes.ts
@@ -20,6 +20,45 @@
 
 import Route from '@ioc:Adonis/Core/Route'
 
-Route.get('/', async ({ view }) => {
-  return view.render('welcome')
+
+//public
+Route.group(() => {
+  Route.get("/", async ({view})=>{
+    return view.render("welcome")
+  })
+  Route.get("/recherche", "")
+  Route.get("/connexion", "")
+  Route.get("/commerce/:slug", "")
 })
+
+
+//user
+Route.group(() => {
+
+  Route.get("/", "")
+
+  Route.group(() => {
+    Route.get("/", "")
+    Route.get("/modifier", "")
+    Route.get("/supprimer", "")
+  }).prefix("/mon-compte")
+
+  Route.group(() => {
+    Route.get("/ajouter", "")
+    Route.group(() => {
+      Route.get("/", "")
+      Route.get("/supprimer", "")
+      Route.group(() => {
+        Route.get("/informations", "")
+        Route.get("/liens", "")
+        Route.get("/categories", "")
+      }).prefix("/modifier")
+    }).prefix("/:slug")
+  }).prefix("/commerce")
+
+}).prefix("/utilisateur")
+
+//admin
+Route.group(() => {
+  Route.get("/", "")
+}).prefix("/admin")
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: [
+    './resources/views/**/*.edge',
+    './resources/views/*.edge',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
