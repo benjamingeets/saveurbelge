b3adb97235f7c89fed2d71153e2977d0892629a7
Benjamin Geets <benjamingeets@tutamail.com>
2022-02-21 22:01:27 +0100

    feat: hide categories if sector unchecked

--- resources/js/app.js
+++ resources/js/app.js
@@ -7,32 +7,40 @@ window.Alpine = Alpine
 
 Alpine.start()
 
+
+const makeTooltip = ({logo,name}) =>{
+    return `
+    <div class="flex p-5">
+        ${logo ? `<img class="h-20" loading="lazy" src="/uploads/${logo}"/>` : ''}
+        <div class="text-2xl">${name}</div>
+    </div>`
+}
+
+const myIcon = L.icon({
+    iconUrl: 'https://cms.galandtristan.be/galandtristan.webp',
+    iconSize: [38, 95],
+    iconAnchor: [22, 94],
+    popupAnchor: [-3, -76],
+    shadowSize: [68, 95],
+    shadowAnchor: [22, 94]
+});
+
 up.link.config.followSelectors.push('a[href]')
 up.form.config.submitSelectors.push(['form'])
+
 up.compiler('#map', () => {
     let lat = 50.850340
     let lon = 4.351710
+    
     const map = L.map('map', { zoomControl: false }).setView([lat, lon], 9);
     map.attributionControl.setPrefix('')
     L.tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png').addTo(map);
-    var myIcon = L.icon({
-        iconUrl: 'https://cms.galandtristan.be/galandtristan.webp',
-        iconSize: [38, 95],
-        iconAnchor: [22, 94],
-        popupAnchor: [-3, -76],
-        shadowSize: [68, 95],
-        shadowAnchor: [22, 94]
-    });
+
     fetch("http://localhost:3333/shops").then((r) => {
         r.json().then((l) => {
             l.forEach(s => {
                 let m = L.marker([s.latitude, s.longitude], { riseOnHover: true }).addTo(map);
-                m.bindTooltip(`
-                <div class="flex p-5">
-                    ${s.logo ? `<img class="h-20" loading="lazy" src="/uploads/${s.logo}"/>` : ''}
-                    <div class="text-2xl">${s.name}</div>
-                </div>`,
-                    { direction: 'center', interactive: true, opacity: 1 });
+                m.bindTooltip(makeTooltip({logo:s.logo,name:s.name}),{ direction: 'center', interactive: true, opacity: 1 });
                 m.on('click', () => {
                     up.visit('/commerce/' + s.slug)
                 })
@@ -44,15 +52,16 @@ up.compiler('#map', () => {
     const input = document.querySelector('input')
     search.addEventListener('submit', (e) => {
         e.preventDefault()
-            fetch('https://api-postcode.vercel.app/get-city/' + input.value).then(r => {
-                r.json().then(o => {
-                    const target = o[0]
-                    map.flyTo(new L.LatLng(target.lat, target.lon), 14)
-                })
+        fetch('https://api-postcode.vercel.app/get-city/' + input.value).then(r => {
+            r.json().then(o => {
+                const target = o[0]
+                map.flyTo(new L.LatLng(target.lat, target.lon), 14)
             })
-    
+        })
     })
 })
+
+
 up.compiler('#edit-shop', () => {
     const bouton = document.querySelector('#logo')
     bouton.addEventListener('change', (e) => {
