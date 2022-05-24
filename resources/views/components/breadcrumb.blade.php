e2b7098e3b96717674b50dc6ad1a0faf8e6e92a7
benjamingeets <benjamingeets@tutamail.com>
2022-05-24 20:30:00 +0200

    fix(): i'm hopeless iOS safari is drunk

--- resources/views/components/breadcrumb.blade.php
+++ resources/views/components/breadcrumb.blade.php
@@ -1,13 +1,13 @@
 @props(['items','current','classes'=>''])
-    <ul class="flex flex-col md:flex-row {{$classes}}">
+    <ul class="flex {{$classes}}">
         @foreach ($items as $item)
-            <li class="flex md:mx-2 after:content-['/']">
+            <li class="flex after:inline-block after:mx-2 after:content-['/']">
                 <a class="text-primary hover:underline" href="{{$item['href']}}">
                     {{$item['label']}}
                 </a>
             </li>
         @endforeach
-        <li class="md:ml-2">
+        <li>
             <span>{{$current}}</span>
         </li>
     </ul>
