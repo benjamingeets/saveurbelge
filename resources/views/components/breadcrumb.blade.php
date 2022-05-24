136d685c2ca986583ecab3157433995adda88608
benjamingeets <benjamingeets@tutamail.com>
2022-05-24 20:12:35 +0200

    fix(homepage): height screen

--- resources/views/components/breadcrumb.blade.php
+++ resources/views/components/breadcrumb.blade.php
@@ -1,13 +1,13 @@
 @props(['items','current','classes'=>''])
-    <ul class="flex {{$classes}}">
+    <ul class="flex flex-col md:flex-row {{$classes}}">
         @foreach ($items as $item)
-            <li class="ml-2 after:content-['/']">
+            <li class="flex md:mx-2 after:content-['/']">
                 <a class="text-primary hover:underline" href="{{$item['href']}}">
                     {{$item['label']}}
                 </a>
             </li>
         @endforeach
-        <li class="ml-2">
+        <li class="md:ml-2">
             <span>{{$current}}</span>
         </li>
     </ul>
