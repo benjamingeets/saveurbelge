bfce7896682c28d8e4e9621fef5f8e93c0483e3a
benjamingeets <benjamingeets@tutamail.com>
2022-05-26 12:29:21 +0200

    fix(db): change description string to text type

---
+++ database/migrations/2022_05_26_102511_update_description_type.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('shops', function (Blueprint $table) {
+            $table->string('description')->change();// Renaming "emp_name" to "employee_name"
+    });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('shops', function (Blueprint $table) {
+            $table->text('description')->change();// Renaming "emp_name" to "employee_name"
+    });
+        //
+    }
+};
