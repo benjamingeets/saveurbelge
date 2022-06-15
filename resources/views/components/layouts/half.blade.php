e9ba2b49324001c395ee887a5c4258a93c0ee2e4
benjamingeets <benjamingeets@tutamail.com>
2022-06-15 13:16:09 +0200

    feat : use Plausible

--- resources/views/components/layouts/half.blade.php
+++ resources/views/components/layouts/half.blade.php
@@ -1,51 +1,48 @@
-@props([
-    'title',
-    'hideFooter'=>false,
-    'sand'=>false,
-    'description'
-    ])
+@props(['title', 'hideFooter' => false, 'sand' => false, 'description'])
 @php
-    if(empty($title)){
-        $title = "SaveurBelge 👨🏻‍🌾 Des produits de chez nous 🥕";
-    }else{
-        $title = $title . " | SaveurBelge";
-    }
-    if(empty($description)){
-        $description = "SaveurBelge | Des produits de chez nous: Carte interactive des producteurs locaux";
-    }
+if (empty($title)) {
+    $title = 'SaveurBelge 👨🏻‍🌾 Des produits de chez nous 🥕';
+} else {
+    $title = $title . ' | SaveurBelge';
+}
+if (empty($description)) {
+    $description = 'SaveurBelge | Des produits de chez nous: Carte interactive des producteurs locaux';
+}
 @endphp
 <!DOCTYPE html>
 <html lang="fr">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">
-    <script async defer src="https://nullitics.com/script.js"></script>
-    <title>{{$title}}</title>
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-        <!-- Primary Meta Tags -->
-        <meta name="description" content="{{$description}}">
+    <script defer data-domain="saveurbelge.be" src="https://plausible.io/js/plausible.js"></script>
 
-        <!-- Open Graph / Facebook -->
-        <meta property="og:type" content="website">
-        <meta property="og:url" content="https://saveurbelge.be/">
-        <meta property="og:title" content="{{$title}}">
-        @if(!empty($title))
-        <title> {{$title}} | SaveurBelge</title>
+    <title>{{ $title }}</title>
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <!-- Primary Meta Tags -->
+    <meta name="description" content="{{ $description }}">
+
+    <!-- Open Graph / Facebook -->
+    <meta property="og:type" content="website">
+    <meta property="og:url" content="https://saveurbelge.be/">
+    <meta property="og:title" content="{{ $title }}">
+    @if (!empty($title))
+        <title> {{ $title }} | SaveurBelge</title>
     @else
         <title>SaveurBelge 👨🏻‍🌾 Des produits de chez nous 🥕</title>
     @endif
-        <meta property="og:description" content="{{$description}}">
-    
-        <!-- Twitter -->
-        <meta property="twitter:card" content="summary_large_image">
-        <meta property="twitter:url" content="https://saveurbelge.be/">
-        <meta property="twitter:title" content="{{$title}}">
-        <meta property="twitter:description" content="{{$description}}">
+    <meta property="og:description" content="{{ $description }}">
 
-        <meta property="twitter:image" content="https://saveurbelge.be/share.webp">
-        <meta property="og:image" content="https://saveurbelge.be/share.webp">
+    <!-- Twitter -->
+    <meta property="twitter:card" content="summary_large_image">
+    <meta property="twitter:url" content="https://saveurbelge.be/">
+    <meta property="twitter:title" content="{{ $title }}">
+    <meta property="twitter:description" content="{{ $description }}">
+
+    <meta property="twitter:image" content="https://saveurbelge.be/share.webp">
+    <meta property="og:image" content="https://saveurbelge.be/share.webp">
 </head>
 
 <body class="flex flex-col min-h-screen">
@@ -55,7 +52,8 @@
             <div class="relative flex flex-col items-center self-stretch justify-center md:w-6/12 md:bg-sand">
                 {{ $slot }}
             </div>
-            <div class="sticky top-0 right-0 items-center justify-center hidden w-6/12 h-screen bg-[url('/background.png')] bg-cover md:flex">
+            <div
+                class="sticky top-0 right-0 items-center justify-center hidden w-6/12 h-screen bg-[url('/background.png')] bg-cover md:flex">
                 <figure>
                     <img class="w-32 h-32 p-3 bg-white rounded-full" src="/saveurbelge.svg" alt="SaveurBelge">
                 </figure>
@@ -64,7 +62,7 @@
 
     </div>
 
-    <x-footer/>
+    <x-footer />
     <script src="{{ asset('js/app.js') }}"></script>
 </body>
 
--- resources/views/components/layouts/main.blade.php
+++ resources/views/components/layouts/main.blade.php
@@ -1,64 +1,62 @@
-@props([
-    'title',
-    'hideFooter'=>false,
-    'sand'=>false,
-    'description',
-    'overflow_hidden'=>false
-    ])
+@props(['title', 'hideFooter' => false, 'sand' => false, 'description', 'overflow_hidden' => false])
 @php
-    if(empty($title)){
-        $title = "SaveurBelge 👨🏻‍🌾 Des produits de chez nous 🥕";
-    }else{
-        $title = $title . " | SaveurBelge";
-    }
-    if(empty($description)){
-        $description = "SaveurBelge | Des produits de chez nous: Carte interactive des producteurs locaux";
-    }
+if (empty($title)) {
+    $title = 'SaveurBelge 👨🏻‍🌾 Des produits de chez nous 🥕';
+} else {
+    $title = $title . ' | SaveurBelge';
+}
+if (empty($description)) {
+    $description = 'SaveurBelge | Des produits de chez nous: Carte interactive des producteurs locaux';
+}
 @endphp
 <!DOCTYPE html>
 <html lang="fr">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">
-    <script async defer src="https://nullitics.com/script.js"></script>
-    <title>{{$title}}</title>
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-        <!-- Primary Meta Tags -->
-        <meta name="description" content="{{$description}}">
+    <script defer data-domain="saveurbelge.be" src="https://plausible.io/js/plausible.js"></script>
 
-        <!-- Open Graph / Facebook -->
-        <meta property="og:type" content="website">
-        <meta property="og:url" content="https://saveurbelge.be/">
-        <meta property="og:title" content="{{$title}}">
-        @if(!empty($title))
-        <title> {{$title}} | SaveurBelge</title>
+    <title>{{ $title }}</title>
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <!-- Primary Meta Tags -->
+    <meta name="description" content="{{ $description }}">
+
+    <!-- Open Graph / Facebook -->
+    <meta property="og:type" content="website">
+    <meta property="og:url" content="https://saveurbelge.be/">
+    <meta property="og:title" content="{{ $title }}">
+    @if (!empty($title))
+        <title> {{ $title }} | SaveurBelge</title>
+        git@github.com:benjamingeets/saveurbelge.git
     @else
         <title>SaveurBelge 👨🏻‍🌾 Des produits de chez nous 🥕</title>
     @endif
-        <meta property="og:description" content="{{$description}}">
-    
-        <!-- Twitter -->
-        <meta property="twitter:card" content="summary_large_image">
-        <meta property="twitter:url" content="https://saveurbelge.be/">
-        <meta property="twitter:title" content="{{$title}}">
-        <meta property="twitter:description" content="{{$description}}">
+    <meta property="og:description" content="{{ $description }}">
 
-        <meta property="twitter:image" content="https://saveurbelge.be/share.webp">
-        <meta property="og:image" content="https://saveurbelge.be/share.webp">
+    <!-- Twitter -->
+    <meta property="twitter:card" content="summary_large_image">
+    <meta property="twitter:url" content="https://saveurbelge.be/">
+    <meta property="twitter:title" content="{{ $title }}">
+    <meta property="twitter:description" content="{{ $description }}">
+
+    <meta property="twitter:image" content="https://saveurbelge.be/share.webp">
+    <meta property="og:image" content="https://saveurbelge.be/share.webp">
 </head>
-<body class="flex flex-col overflow-x-hidden  @if($overflow_hidden) h-screen overflow-hidden @else min-h-screen @endif @if($sand) lg:bg-sand @endif">
-    <x-navbar/>
+
+<body
+    class="flex flex-col overflow-x-hidden  @if ($overflow_hidden) h-screen overflow-hidden @else min-h-screen @endif @if ($sand) lg:bg-sand @endif">
+    <x-navbar />
     <div class="relative flex flex-col flex-1">
-        {{$slot}}
+        {{ $slot }}
     </div>
 
-    @unless ($hideFooter)
-    <x-footer/>
+    @unless($hideFooter)
+        <x-footer />
     @endunless
-    <script src="{{asset('js/app.js')}}">
-
-    </script>
+    <script src="{{ asset('js/app.js') }}"></script>
 </body>
+
 </html>
